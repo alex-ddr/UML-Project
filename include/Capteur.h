960d9d22c667671f9620a588255e5051deb7e67c
Alexandre Didier <atrdidier@gmail.com>
2025-05-14 15:08:15 +0200

    coté capteur

---
+++ include/Capteur.h
@@ -0,0 +1,41 @@
+#ifndef CAPTEUR_H
+#define CAPTEUR_H
+using namespace std;
+
+#include <string>
+
+class Capteur
+{
+private:
+  long capteur_id;
+  float latitude;
+  float longitude;
+  bool confiance;
+  bool prive;
+
+public:
+  // Constructor
+  Capteur(long id, float lat, float lon, bool conf, bool priv)
+      : capteur_id(id), latitude(lat), longitude(lon), confiance(conf), prive(priv) {}
+
+  // Méthodes
+  bool estCapteurDeConfiance();
+
+  // Getters
+  long getCapteurId() const { return capteur_id; }
+  float getLatitude() const { return latitude; }
+  float getLongitude() const { return longitude; }
+  bool isConfiance() const { return confiance; }
+  bool isPrive() const { return prive; }
+
+  // Setters
+  void setCapteurId(long id) { capteur_id = id; }
+  void setLatitude(float lat) { latitude = lat; }
+  void setLongitude(float lon) { longitude = lon; }
+  void setConfiance(bool conf) { confiance = conf; }
+  void setPrive(bool priv) { prive = priv; }
+
+  // Destructor
+  ~Capteur() {}
+};
+#endif
---
+++ include/Mesures.h
@@ -0,0 +1,33 @@
+#ifndef CAPTEUR_H
+#define CAPTEUR_H
+using namespace std;
+
+#include <ctime>
+#include <Attribut.h>
+
+class Mesures
+{
+private:
+  time_t timestamp;
+  float valeur;
+  Attribut attribut;
+
+public:
+  // Constructors
+  Mesures();
+  Mesures(time_t timestamp, float valeur, Attribut attribut);
+
+  // Getters
+  time_t getTimestamp() const;
+  float getValeur() const;
+  Attribut getAttribut() const;
+
+  // Setters
+  void setTimestamp(time_t timestamp);
+  void setValeur(float valeur);
+  void setAttribut(Attribut attribut);
+
+  // Destructor
+  ~Mesures();
+};
+#endif
