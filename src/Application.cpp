d937da67528c790ca8aa148b244b2339d7028b0a
Alexandre Didier <atrdidier@gmail.com>
2025-05-14 16:31:47 +0200

    demande de modif application

--- src/Application.cpp
+++ src/Application.cpp
@@ -15,5 +15,22 @@ float Application::moyenneQualiteAir(float latitude, float longitude, time_t deb
 
 Capteur *Application::listerCapteursSimilaires(Capteur &capteur) const
 {
-    Mesures *liste_mesures_capteur = capteur
+    // 1. Date la plus récente dans toutes les mesures
+    vector<Mesures> liste_mesures_capteur = capteur.getListeMesures();
+    time_t date_max = NULL;
+    for (const auto &mesure : liste_mesures_capteur)
+        if (!date_max || date_max < mesure.getTimestamp())
+            date_max = mesure.getTimestamp();
+
+    // 2. Date de début
+    time_t date_debut = date_max - 7 * 24 * 60 * 60;
+
+    // 3. Toutes les mesures de capteur entre date_debut et date_max
+    vector<Mesures> liste_mesures;
+    for (const auto &mesure : liste_mesures)
+        if (mesure.getTimestamp() > date_debut)
+            liste_mesures.push_back(mesure);
+
+    // 4. Liste vide
+    vector<Capteur> capteurs_similaires;
 }
